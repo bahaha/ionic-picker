c81e8b996fece880af2c86cfff6dd5174bca5c6b
Cheng-Che Cheng <clementcheng56@gmail.com>
2017-05-02 23:45:49 +0800

    provide well-format data if not using PickerData

--- www/js/app.js
+++ www/js/app.js
@@ -47,7 +47,7 @@ angular.module('starter', ['ionic', 'picker'])
           items: [{label:"Second1",value:0},{label:"Second2",value:1},{label:"Second3",value:2},{label:"Second4",value:3},{label:"Second5",value:4},{label:"Second6",value:5},{label:"Second7",value:6},{label:"Second8",value:7},{label:"Second9",value:8}]
         },
         {
-          items: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+          items: [{label: '1', value: 1}, {label: '2', value: 2}, {label: '3', value: 3}, {label: '4', value: 4}, {label: '5', value: 5}, {label: '6', value: 6}, {label: '7', value: 7}, {label: '8', value: 8}, {label: '9', value: 9}, {label: '10', value: 10}, {label: '11', value: 11}, {label: '12', value: 12}, {label: '13', value: 13}, {label: '14', value: 14}, {label: '15', value: 15}]
         }
       ]
     });
